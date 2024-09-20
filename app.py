118c49367c8efb15861cfee75091f13db1cf7999
Atta-create <attaullahkhan5440@gmail.com>
2024-09-20 13:36:39 +0500

    Your commit message

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify 
 app = Flask(__name__)
-
 JOBS = [
         {
       'id':1,
@@ -27,10 +26,33 @@ JOBS = [
       'salary': 'Rs. 50,000'
        }
     ]
-   
+ 
 @app.route("/")
 def hello_SDSS():
-    return render_template ('home.html', jobs=JOBS)
-if __name__=="__main__":
+    return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+   return jsonify(JOBS)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/research')
+def research():
+    return render_template('research.html')
+
+@app.route('/get_involved')
+def get_involved():
+    return render_template('get_involved.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__=='__main__':
   app.run(host='0.0.0.0', debug=True)
   
